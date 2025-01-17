3085767a936d165000b0aae4f8b3792cc858f8c5
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-17 20:13:33 +0500

    fourth  commit

--- labb1.py
+++ labb1.py
@@ -5,3 +5,13 @@ if 5 > 2:
         print("Five is greater than two!") 
 x = 5
 y = "Hello, World!"
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
