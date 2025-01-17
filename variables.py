38fe018c9ee2432ef51514aa2b192246b1f38d4c
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-17 22:19:31 +0500

    first commit

--- variables.py
+++ variables.py
@@ -20,3 +20,51 @@ print(type(y))
 x = "John"
 # is the same as
 x = 'John'
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+
+x = 5
+y = 10
+print(x + y)
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
