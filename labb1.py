fcc2205f59e718dcca1ee1909f2f472b3cc98523
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-17 20:19:20 +0500

    fifth commit

--- labb1.py
+++ labb1.py
@@ -15,3 +15,14 @@ written in
 more than just one line
 """
 print("Hello, World!")
+x = 5
+y = "John"
+print(x)
+print(y)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
