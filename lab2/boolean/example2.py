69e97200e7b08795ed812067ea2cca8176914873
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-23 22:23:47 +0500

    first commit

---
+++ lab2/boolean/example2.py
@@ -0,0 +1,17 @@
+print(bool("Hello"))
+print(bool(15))
+
+
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
