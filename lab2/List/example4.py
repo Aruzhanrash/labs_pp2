6cf4fac3eea9923ae372506e84c7f7559fb96ff8
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-23 22:45:02 +0500

    commit

---
+++ lab2/List/example4.py
@@ -0,0 +1,18 @@
+#Using the append() method to append an item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+#Insert an item as the second position:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+#Add the elements of tropical to thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
