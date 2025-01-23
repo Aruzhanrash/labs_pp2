0b1fea960f55b7e24b82c97e9ef914da51a322c3
Aruzhanrash <ar_rashit@kbtu.kz>
2025-01-23 23:02:23 +0500

    commit

---
+++ lab2/Tuples/example1.py
@@ -0,0 +1,18 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+#Print the number of items in the tuple:
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#One item tuple, remember the comma:
+thistuple = ("apple",)
+print(type(thistuple))
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+#String, int and boolean data types:
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
